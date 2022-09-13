011750bc665e3aad76dc98ad3c858a980ed84695
Manuel Castanares <manuel.castanares14@gmail.com>
2022-09-13 15:20:49 -0300

    Melhorando jogo

--- Rungun/Assets/Scripts/PlayerController.cs
+++ Rungun/Assets/Scripts/PlayerController.cs
@@ -5,26 +5,43 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     public float walkSpeed;
     public float jumpHeight;
+    public LayerMask mask;
+    private float moveVelocity;
     Rigidbody2D rb;
+    private bool _canJump;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent <Rigidbody2D> ();
+        _canJump = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetAxis("Jump") > 0 && IsGrounded()) {
-            print("pulei");
+        if(Input.GetAxis("Jump") > 0 && IsGrounded() && _canJump) {
             rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+            _canJump = false;
+            Invoke("setCanJump", 0.8f);
         }
+
+        moveVelocity = 0;
+        if(Input.GetAxis("Horizontal") != 0){
+            if(Input.GetAxis("Horizontal") > 0){
+                moveVelocity += walkSpeed;
+            } else {
+                moveVelocity -= walkSpeed;
+            }
+        }
+        rb.velocity = new Vector2(moveVelocity, rb.velocity.y);
+    }
+
+    void setCanJump() {
+        this._canJump = true;
     }
 
     private bool IsGrounded() {
-        
-       var groundCheck = Physics2D.Raycast(transform.position, Vector2.down, 0.7f);
-       print(groundCheck.collider.tag);
+       var groundCheck = Physics2D.Raycast(transform.position, Vector2.down, 0.7f, mask);
        return groundCheck.collider != null && groundCheck.collider.CompareTag("Ground");
     }
 }
