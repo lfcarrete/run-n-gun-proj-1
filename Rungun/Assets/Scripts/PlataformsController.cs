678b73d6dc39e3e9676f40d9a69d1c5b0cd0ac5b
Manuel Castanares <manuel.castanares14@gmail.com>
2022-09-23 07:43:36 -0300

    sprites

--- Rungun/Assets/Scripts/PlataformsController.cs
+++ Rungun/Assets/Scripts/PlataformsController.cs
@@ -8,10 +8,14 @@ public class PlataformsController : MonoBehaviour
     private GameObject player;
     public float timeDestruction;
     public GameObject platform;
+    public SpriteRenderer spriteRenderer;
+    public Sprite newSprite;
+    public Sprite newSprite2;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent <Rigidbody2D> ();
+
         player = GameObject.FindWithTag("Player");
     }
 
@@ -23,9 +27,21 @@ public class PlataformsController : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D col){
         Invoke("DestroyPlatform", timeDestruction);
+        Invoke("ChangeSprite", timeDestruction/3);
+        Invoke("ChangeSpriteAgain", timeDestruction*2/3);
+
     } 
 
     void DestroyPlatform(){
         Destroy(platform);
     }
-}
+
+    void ChangeSprite(){
+        spriteRenderer.sprite = newSprite;
+
+    }
+
+    void ChangeSpriteAgain(){
+        spriteRenderer.sprite = newSprite2;
+
+    }}
